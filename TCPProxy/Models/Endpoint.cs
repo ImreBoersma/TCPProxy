1dd03ebc5ff98eba6293bb9a177fb61616722a51
Imre Boersma <imre.boersma@gmail.com>
2023-03-03 11:34:27 +0100

    Improved Endpoint conversion

--- TCPProxy/Models/Endpoint.cs
+++ TCPProxy/Models/Endpoint.cs
@@ -27,7 +27,7 @@ public record Endpoint
                 }
                 catch (SocketException e)
                 {
-                    Log.Error(e, "Failed to resolve host {Host}", splitAddress);
+                    Log.Error("Failed to resolve host {Host}:\r\n{Exception}", splitAddress, e.Message);
                     return;
                 }
             }
@@ -57,31 +57,53 @@ public record Endpoint
     /// </summary>
     /// <param name="endpoint">The endpoint to convert</param>
     /// <returns>The converted endpoint</returns>
-    public static implicit operator IPEndPoint(Endpoint endpoint)
+    public static implicit operator IPEndPoint?(Endpoint endpoint)
     {
-        var hostEntry = Dns.GetHostEntry(endpoint.Host);
-        var ipAddresses = hostEntry.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetwork).ToList();
-
-        if (!ipAddresses.Any())
+        try
         {
-            Log.Warning("No valid addresses found for {Host}", endpoint.Host);
-            return new IPEndPoint(IPAddress.Loopback, 80);
+            var hostEntry = Dns.GetHostEntry(endpoint.Host);
+            var ipAddresses = hostEntry.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+            if (!ipAddresses.Any())
+            {
+                Log.Warning("No valid addresses found for {Host}", endpoint.Host);
+            }
+
+            if (ipAddresses.Count == 1) return new IPEndPoint(ipAddresses.First(), endpoint.Port);
+
+            Log.Warning("\tMultiple addresses found for {Host}:", endpoint.Host);
+            foreach (var (address, index) in ipAddresses.Select((v, i) => (v, i)))
+            {
+                var connected = TryConnectToEndpoint(new IPEndPoint(address, endpoint.Port));
+                Log.Debug("\t\t{Index}: {Address} - {Connected}", index, address.ToString(), connected ? "Connected" : "Not connected");
+                if (connected)
+                {
+                    Log.Debug("\t\tSelected {Address} to forward request to", address.ToString());
+                    return new IPEndPoint(address, endpoint.Port);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error("Failed to resolve host {Host}:\r\n{Exception}", endpoint.Host, e.Message);
         }
 
-        if (ipAddresses.Count == 1)
+        return null;
+    }
+
+    private static bool TryConnectToEndpoint(IPEndPoint endpoint)
+    {
+        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
         {
-            var address = ipAddresses.First();
-            Log.Debug("Selected {Address} to forward request to", address.ToString());
-            return new IPEndPoint(address, endpoint.Port);
+            socket.Connect(endpoint);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.Error("Error connecting to server {ServerIp}\r\n{Exception}", endpoint.ToString(), e.Message);
         }
 
-        Log.Warning("Multiple addresses found for {Host}:", endpoint.Host);
-        foreach (var (address, index) in ipAddresses.Select((v, i) => (v, i)))
-        {
-            Log.Debug("{Index}: {Address}", index, address.ToString());
-            return new IPEndPoint(address, endpoint.Port);
-        }
-
-        return new IPEndPoint(IPAddress.Loopback, endpoint.Port);
+        return false;
     }
 }
